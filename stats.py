240c3608fe30e8edba64116aba2b9758739da448
ashwin618 <ashwin2019618@gmail.com>
2022-02-20 20:38:23 -0500

    Initial commit

---
+++ stats.py
@@ -0,0 +1,14 @@
+from mcstatus import MinecraftServer
+
+ipserver = input('Server? ')
+
+server = MinecraftServer.lookup(ipserver)
+
+status = server.status()
+print(f"The server has {status.players.online} players and replied in {status.latency} ms")
+
+latency = server.ping()
+print(f"The server replied in {latency} ms")
+
+query = server.query()
+print(f"The server has the following players online: {', '.join(query.players.names)}")
